2912a3cfe4a52d1abe25fee024497f7d1f8fdf9b
Christophe Marchal <ma-ch@kichechef.lu>
2016-01-13 15:17:54 +0100

    first release

---
+++ spotifycmd/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace spotifycmd
+{
+    static class Program
+    {
+        const int WM_APPCOMMAND = 0x0319;
+
+        const long SPOTIFY_NEXT = 720896;
+        const long SPOTIFY_PLAYPAUSE = 917504;
+        const long SPOTIFY_MUTE = 524288;
+        const long SPOTIFY_VOLUP = 655360;
+        const long SPOTIFY_VOLDOWN = 589824;
+        const long SPOTIFY_STOP = 851968;
+        const long SPOTIFY_PREV = 786432;
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length < 1) return;
+
+            IntPtr hWnd = FindWindow("SpotifyMainWindow", null);
+
+            if (hWnd.ToInt32() != 0)
+            {
+                long cmd = 0;
+                switch (args[0])
+                {
+                    case "next":
+                        cmd = SPOTIFY_NEXT;
+                        break;
+                }
+
+                IntPtr param = new IntPtr(cmd);
+
+                SendMessage(hWnd, WM_APPCOMMAND, IntPtr.Zero, param);
+            }
+
+        }
+    }
+}
---
+++ spotifycmd/README.md
@@ -0,0 +1,10 @@
+﻿# Spotify command line tool
+
+Un bête programme pour controler Spotify depuis la ligne de commande.
+
+Ca me permet de fixer dans la base des registre la ligne de commande pour mon clavier multimédia sans installer ses drivers.
+
+Moi c'est dans 
+`HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AppKey`
+
+Reste à trouver, la bonne clé pour la bonne touche du clavier
